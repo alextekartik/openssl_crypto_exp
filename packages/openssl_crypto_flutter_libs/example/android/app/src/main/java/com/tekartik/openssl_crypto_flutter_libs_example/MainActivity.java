a5fbb4f6cfa61de4759c764acd9ed182a0df929f
alex <alex@tekartik.com>
2021-11-09 09:13:35 +0100

    fix android build

---
+++ packages/openssl_crypto_flutter_libs/example/android/app/src/main/java/com/tekartik/openssl_crypto_flutter_libs_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.tekartik.openssl_crypto_flutter_libs_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- packages/openssl_crypto_flutter_libs/example/android/app/src/main/kotlin/com/tekartik/openssl_crypto_flutter_libs_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.tekartik.openssl_crypto_flutter_libs_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
